86bfba4f7fd5abc3e3a7fdf0d380aac0d11d20ce
Bravin6 <bravinwekhuyi@gmail.com>
2024-06-04 11:03:06 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,29 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """
 Script to print hot posts on a given Reddit subreddit.
 """
-[200~import requests
+import requests
 
-        def top_ten(subreddit):
-            url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-                headers = {
-                            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-                                }
-                    params = {
-                                "limit": 10
-                                    }
-                        response = requests.get(url, headers=headers, params=params)
-                            if response.status_code == 404:
-                                    print("Subreddit not found.")
-                                            return
-                                                data = response.json().get("data", {}).get("children", [])
-                                                    for post in data:
-                                                            print(post["data"]["title"])
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 404:
+        print("Subreddit not found.")
+        return
+    results = response.json().get("data")
+    for post in results.get("children", []):
+        print(post.get("data").get("title"))
 
-                                                            if __name__ == "__main__":
-                                                                import sys
-                                                                    if len(sys.argv) < 2:
-                                                                            print("Please pass an argument for the subreddit to search.")
-                                                                                else:
-                                                                                        top_ten(sys.argv[1])
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
 
